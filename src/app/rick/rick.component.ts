6efe677088bda67324d304a3aee168ec5abedb2f
Gonzalo Ibarra <gibarra@activeit.cl>
2021-04-14 15:19:41 -0400

    componente rick

---
+++ src/app/rick/rick.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rick',
+  templateUrl: './rick.component.html',
+  styleUrls: ['./rick.component.css']
+})
+export class RickComponent implements OnInit {
+
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
